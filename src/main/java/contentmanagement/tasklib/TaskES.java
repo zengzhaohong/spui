13bfa04f1aed5ce6a9af71c11ff94bf650763a37
linhaihua1 <944963047@qq.com>
2021-12-06 17:30:08 +0800

    提交征稿库

--- src/main/java/contentmanagement/tasklib/TaskES.java
+++ src/main/java/contentmanagement/tasklib/TaskES.java
@@ -3,34 +3,63 @@ package contentmanagement.tasklib;
 import common.ElementStore;
 
 public class TaskES extends ElementStore {
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	//
-	public static final String 
-	
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	//
+//	public static final String 
+//	
 
 }
