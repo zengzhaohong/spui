74dc3df1e69e2e42b763281fc5316162695dffa6
linhaihua1 <944963047@qq.com>
2021-12-06 17:25:56 +0800

    增加任务库

---
+++ src/main/java/contentmanagement/tasklib/TaskES.java
@@ -0,0 +1,36 @@
+package contentmanagement.tasklib;
+
+import common.ElementStore;
+
+public class TaskES extends ElementStore {
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	//
+	public static final String 
+	
+
+}
